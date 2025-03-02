8dc1f0efdc2d8c442b4337a483fefab3bbcfaf9c
JosephVTX <jvcallupe69@gmail.com>
2025-03-01 23:37:57 -0500

    Agregar rutas de Artisan para migración y enlace de almacenamiento [deploy]

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', fn() => response()->json(['message' => 'Hello World!']));
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::resource('products', ProductController::class);
     Route::resource('categories', CategoryController::class);
+
+    Route::group(['prefix' => 'artisan'], function () {
+        Route::get('/migrate', function () {
+            Artisan::call('migrate:fresh --seed');
+            return response()->json(['message' => 'Database migrated and seeded successfully']);
+        });
+        Route::get('/storage:link', function () {
+            Artisan::call('storage:link');
+            return response()->json(['message' => 'Storage linked successfully']);
+        });
+    });
 });
+
+
 
 require __DIR__ . '/auth.php';  
